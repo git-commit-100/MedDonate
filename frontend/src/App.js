dff4f74b809e11446a39bf66ef5ac709f7891048
Java Monk <javamonk.coder@gmail.com>
2022-11-01 12:28:21 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,7 @@
-import { useState } from "react";
-
 function App() {
-  const [data, setData] = useState("Null");
-
-  async function fetchData() {
-    // endpoint for server
-    const response = await fetch("http://localhost:8080");
-
-    const body = await response.json();
-
-    setData(body.response.toString());
-  }
-
-  return (
-    <div className="App">
-      <h2 className="response">{data}</h2>
-      <br />
-      <button onClick={fetchData}>Get Data</button>
-    </div>
-  );
+  return <div className="App">
+    <h1>React Application</h1>
+  </div>;
 }
 
 export default App;
